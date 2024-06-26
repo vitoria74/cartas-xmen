ba888f4c649df43985f0d2a303cd6974cc075437
vitoria74 <maria_vitoria74@hotmail.com>
2024-06-26 15:25:59 -0300

    retirar selecionado js

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,15 @@
  const btnAvancar = document.getElementById('btn-avancar');
+
  let cartaoAtual = 0;
 
  btnAvancar.addEventListener('click', function(){
+
+    const cartaoSelecionado = document.querySelector('.selecionado');
+    cartaoSelecionado.classList.remove('selecionado');
+
     const cartoes = document.querySelectorAll('.cartao'); //selecionar todos os cartoes da classe .cartao
     cartaoAtual++;
     cartoes[cartaoAtual].classList.add('selecionado');
 
-    
+
 });
