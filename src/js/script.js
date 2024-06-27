f1eacba998dff54180a103941aa179f5ea05c270
vitoria74 <maria_vitoria74@hotmail.com>
2024-06-27 12:22:34 -0300

    jsdoc adicionado

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,17 @@
+ /**
+  * @description Código para as setas "avançar para próxima carta" e "voltar para carta anterior"
+  */
+ 
  const btnAvancar = document.getElementById('btn-avancar');
  const btnVoltar = document.getElementById('btn-voltar');
 
  let cartaoAtual = 0;
 
  const cartoes = document.querySelectorAll('.cartao'); 
+
+ /**
+  * @description Função para a seta "avançar".
+  */
 
  btnAvancar.addEventListener('click', function(){
 
@@ -16,6 +24,10 @@
 
 });
 
+/**
+ * @description Função para a seta "voltar".
+ */
+
 btnVoltar.addEventListener('click', function(){
 
     if(cartaoAtual === 0) return;
@@ -27,9 +39,17 @@ btnVoltar.addEventListener('click', function(){
 
 });
 
+/**
+ * @description Função que torna a carta atual como ".selecionado" para mostrá-la
+ */
+
 function mostrarCartao() {
     cartoes[cartaoAtual].classList.add('selecionado');
 }
+
+/**
+ * @description Função para retirar a classe "selecionado" da carta anterior
+ */
 
 function esconderCartaoSelecionado() {
     const cartaoSelecionado = document.querySelector('.selecionado');
